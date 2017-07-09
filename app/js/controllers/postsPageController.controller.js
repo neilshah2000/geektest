06511edb45900782f84d03d87745611502f7fc1e
neilshah2000 <neil.shah@thomsonreuters.com>
2017-07-09 15:37:43 +0100

    sort

--- app/js/controllers/postsPageController.controller.js
+++ app/js/controllers/postsPageController.controller.js
@@ -12,11 +12,10 @@
   
         vm.postData = [];
         vm.userData = [];
+        vm.combinedData = []
         vm.posts = posts;
         vm.users = users;
 
-        vm.alphabetical = alphabetical;
-        vm.recomended = recomended;
         vm.$onInit = onInit;
         vm.$onChanges = onChanges;
         
@@ -29,24 +28,18 @@
             posts().then(users).then(function(){
                 console.log(vm.postData);
                 console.log(vm.userData);
-            })
+                combineData();
+                alphabetical()
+            });
         }
 
         function posts(){
-            // DataService.getPosts().then(function(data){
-            //     vm.posts = data;
-            //     console.log(data);
-            // })
             return DataService.getPosts().then(function(data){
                 vm.postData = data;
             });
         }
 
         function users(){
-            // DataService.getUsers().then(function(data){
-            //     vm.users = data;
-            //     console.log(vm.users);
-            // });
             return DataService.getUsers().then(function(data){
                 vm.userData = data;
             });
@@ -54,28 +47,28 @@
 
         function getUser(mUserId){
             var user;
-            user = vm.users.find(function(user){
+            user = vm.userData.find(function(user){
                 return user.userId = mUserId;
             });
             return user;
         }
 
+        function combineData(){
+            vm.postData.forEach(function(post){
+                var user = getUser(post.userId);
+                post.user = user;
+            });
+        }
+
         function alphabetical(){
-            vm.venues.sort(alphabeticalCompare);
+            vm.postData.sort(alphabeticalCompare);
 
             function alphabeticalCompare(r1, r2){
-                if(r1.venue.name < r2.venue.name) return -1;
+                if(r1.title < r2.title) return -1;
                 else return 1;
             }
         }
 
-        function recomended(){
-            vm.venues.sort(recomendedCompare);
-
-            function recomendedCompare(r1, r2){
-                return (r2.venue.rating - r1.venue.rating)*10;
-            }
-        }
 
         function onInit(){
             // console.log(vm);
